2ee318aa36569d164d9a051ac6170f4515dea60e
Sergio Bustos <sbustos95@gmail.com>
2025-03-21 08:00:31 -0500

    Cambio de imagenes

--- tarotonline.eltarotuniversal.com/src/data/majorArcana.ts
+++ tarotonline.eltarotuniversal.com/src/data/majorArcana.ts
@@ -1,134 +1,134 @@
 export const majorArcana = [
   {
     id: 0,
-    name: "El Loco",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/0.jpg",
+    name: "El Carro",
+    image: "/tarot-cards/el-carro.png",
     description: "Nuevo comienzo, espontaneidad, fe en el universo. Representa la libertad y el inicio de un viaje espiritual."
   },
   {
     id: 1,
-    name: "El Mago",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/1.jpg",
+    name: "El Colagado",
+    image: "/tarot-cards/el-colgado.png",
     description: "Poder personal, habilidad, concentración. Dominio de los elementos y manifestación de deseos."
   },
   {
     id: 2,
-    name: "La Sacerdotisa",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/2.jpg",
+    name: "El Demonio",
+    image: "/tarot-cards/el-demonio.png",
     description: "Intuición, misterio, sabiduría interior. Guardiana de los secretos y conocimiento oculto."
   },
   {
     id: 3,
-    name: "La Emperatriz",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/3.jpg",
+    name: "El Emperador",
+    image: "/tarot-cards/el-emperador.png",
     description: "Fertilidad, abundancia, creatividad. Representa la madre naturaleza y el poder femenino."
   },
   {
     id: 4,
-    name: "El Emperador",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/4.jpg",
+    name: "El Hermitaño",
+    image: "/tarot-cards/el-hermitaño.png",
     description: "Autoridad, estructura, poder. Simboliza el liderazgo y el control sobre las situaciones."
   },
   {
     id: 5,
-    name: "El Hierofante",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/5.jpg",
+    name: "El Papa",
+    image: "/tarot-cards/el-papa.png",
     description: "Tradición, educación, creencias. Guía espiritual y maestro de conocimientos sagrados."
   },
   {
     id: 6,
-    name: "Los Enamorados",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/6.jpg",
+    name: "La Estrella",
+    image: "/tarot-cards/la-estrella.png",
     description: "Elección, amor, armonía. Representa decisiones importantes y conexiones profundas."
   },
   {
     id: 7,
-    name: "El Carro",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/7.jpg",
+    name: "La Fuerza",
+    image: "/tarot-cards/la-fuerza.png",
     description: "Victoria, determinación, avance. Simboliza el progreso y el control sobre el destino."
   },
   {
     id: 8,
-    name: "La Fuerza",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/8.jpg",
+    name: "La Justicia",
+    image: "/tarot-cards/la-justicia.png",
     description: "Coraje, paciencia, control interior. Dominio de los instintos y poder personal."
   },
   {
     id: 9,
-    name: "El Ermitaño",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/9.jpg",
+    name: "La Muerte",
+    image: "/tarot-cards/la-muerte.png",
     description: "Introspección, búsqueda, soledad. Guía interior y sabiduría profunda."
   },
   {
     id: 10,
     name: "La Rueda de la Fortuna",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/10.jpg",
+    image: "/tarot-cards/la-rueda-de-la-fortuna.png",
     description: "Cambio, destino, ciclos. Los giros inesperados de la vida y el karma."
   },
   {
     id: 11,
-    name: "La Justicia",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/11.jpg",
+    name: "La Templanza",
+    image: "/tarot-cards/la-templanza.png",
     description: "Equilibrio, verdad, consecuencias. Karma y decisiones justas."
   },
   {
     id: 12,
-    name: "El Colgado",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/12.jpg",
+    name: "La Torre",
+    image: "/tarot-cards/la-torre.png",
     description: "Sacrificio, perspectiva, rendición. Nueva visión a través del abandono."
   },
   {
     id: 13,
-    name: "La Muerte",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/13.jpg",
+    name: "Los Enamoraddos",
+    image: "/tarot-cards/los-enamorados.png",
     description: "Transformación, fin, renacimiento. Cambios profundos y nuevos comienzos."
   },
   {
     id: 14,
     name: "La Templanza",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/14.jpg",
+    image: "/tarot-cards/el-carro.png",
     description: "Balance, moderación, armonía. Integración de opuestos y sanación."
   },
   {
     id: 15,
     name: "El Diablo",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/15.jpg",
+    image: "/tarot-cards/el-carro.png",
     description: "Ataduras, materialismo, sombras. Enfrentar los aspectos oscuros y las tentaciones."
   },
   {
     id: 16,
     name: "La Torre",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/16.jpg",
+    image: "/tarot-cards/el-carro.png",
     description: "Destrucción, revelación, liberación. Cambios repentinos y despertar."
   },
   {
     id: 17,
     name: "La Estrella",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/17.jpg",
+    image: "/tarot-cards/el-carro.png",
     description: "Esperanza, inspiración, guía. Renovación de la fe y conexión divina."
   },
   {
     id: 18,
     name: "La Luna",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/18.jpg",
+    image: "/tarot-cards/el-carro.png",
     description: "Ilusión, intuición, miedos. Navegando por lo desconocido y los sueños."
   },
   {
     id: 19,
     name: "El Sol",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/19.jpg",
+    image: "/tarot-cards/el-carro.png",
     description: "Alegría, éxito, vitalidad. Claridad y logros positivos."
   },
   {
     id: 20,
     name: "El Juicio",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/20.jpg",
+    image: "/tarot-cards/el-carro.png",
     description: "Renacimiento, llamado, despertar. Evaluación y transformación espiritual."
   },
   {
     id: 21,
     name: "El Mundo",
-    image: "https://raw.githubusercontent.com/adriancast/Tarot-Deck/master/src/assets/21.jpg",
+    image: "/tarot-cards/el-carro.png",
     description: "Completitud, logro, integración. El final de un ciclo y realización."
   }
 ];
